018e6a7ad15240ae11f49dfd7fea86aa045ceb7e
Mateus-teus <114873188+Mateus-teus@users.noreply.github.com>
2022-12-24 17:44:34 -0300

    exercicios

--- aula14.ex/ex016/Modelo/script.js
+++ aula14.ex/ex016/Modelo/script.js
@@ -5,8 +5,28 @@ function contar(){
     let res = document.getElementById('res')
   
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0){
-       window.alert('[Erro] Faltam dados!')
+       //window.alert('[Erro] Faltam dados!')
+       res.innerHTML = `Impossível contar!`
     }else{
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <=0 ){
+            window.alert('Passo Invalido! Considerando Passo 1')
+            p = 1
+        }
+        if (i < f){
+            // contagem crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1f449}`
+                }
+                
+        }else{
+            // Contagem regressiva
+            for(let c = i; c >= f; c-= p){
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }res.innerHTML += `\u{1f3c1}`
     }
 }
